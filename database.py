1c98c3029b2244261fc5ec26a2327468c650ea35
vanishedbydefa <128320031+vanishedbydefa@users.noreply.github.com>
2023-12-07 20:41:20 +0100

    Add files via upload

---
+++ database.py
@@ -0,0 +1,66 @@
+import sqlite3
+import os
+
+def check_db_entry_exists(db_path:str, id_value:int):
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    # Execute a SELECT query to check if the entry exists
+    cursor.execute('SELECT * FROM img_data WHERE id = ?', (id_value,))
+    result = cursor.fetchone()  # Fetches the first row that matches the condition
+
+    conn.close()
+
+    # Check if the result is not None (entry exists) or None (entry does not exist)
+    return result is not None
+
+# Function to insert data into the database
+def insert_or_update_entry(db_path:str, id_value:int , name:str, category:str, download_date, url:str):
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    # Check if the entry exists
+    cursor.execute('SELECT * FROM img_data WHERE id = ?', (id_value,))
+    existing_entry = cursor.fetchone()
+
+    if existing_entry:
+        # If the entry exists, update it
+        cursor.execute('''
+            UPDATE img_data
+            SET name=?, category=?, download_date=?, url=?
+            WHERE id=?
+        ''', (name, category, download_date, url, id_value))
+    else:
+        # If the entry does not exist, insert a new one
+        cursor.execute('''
+            INSERT INTO img_data (id, name, category, download_date, url)
+            VALUES (?, ?, ?, ?, ?)
+        ''', (id_value, name, category, download_date, url))
+
+    conn.commit()
+    conn.close()
+
+# Function to create a new database if it doesn't exist
+def create_new_db(db_path):
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS img_data (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            category TEXT,
+            download_date DATE,
+            url TEXT
+        )
+    ''')
+    conn.commit()
+    conn.close()
+    
+# Function to check if the database exists
+def check_db_exists(db_path):
+    if not os.path.exists(db_path):
+        print("    - Database not found. Creating a new one...")
+        create_new_db(db_path)
+        print("    - Database created at", db_path)
+    else:
+        print("    - Database already exists at", db_path)
---
+++ helper.py
@@ -0,0 +1,102 @@
+import os
+import time
+import re
+import requests
+from bs4 import *
+
+from database import check_db_exists
+
+def get_time():
+    return time.strftime('[%H:%M:%S]')
+
+def get_timestamp():
+    return time.time()
+
+def create_urls(images:int):
+    return [[i,"https://faproulette.co/"+str(i)] for i in range(1, images+1)]
+
+
+def check_path_exists(path:str, create=False):
+    '''
+    Check wether path exists or not.
+    If path do not exist but create=True,
+    create a directory.
+    '''
+    path_exist = os.path.exists(path)
+    if path_exist:
+        return True
+
+    #path do not exist, try to create it
+    if create:
+        try:
+            os.makedirs(path)
+            return True
+        except OSError as e:
+            print(f"Failed to create directory: {path}")
+            print(f"Error: {e}")
+            return False
+    return False
+
+
+def initial_checks(param_path:str, db_path:str):
+    ##check specified path to folder and create if it do not exist
+    if check_path_exists(param_path, create=True):
+        print(f'    - Folder path at {param_path} is valid')
+    else:
+        print(f"\nError:\nThe folder where to store downloaded images "
+            f"do not exist and couldn't be created. Please check \n"
+            f"the correctness of the provided path: "
+            f"{param_path}.")
+        exit(1)
+    
+    ##check if the db exist and create if it do not exist
+    check_db_exists(db_path)
+
+
+def process_url(url:list): 
+    # content of URL
+    r = requests.get(url)
+    # Parse HTML Code
+    soup = BeautifulSoup(r.text, 'html.parser')
+    # find all images in URL
+    images = soup.findAll('img')
+    
+    # Find title
+    title = soup.find('title')
+    if not title:
+        title = "faproulette"
+    else:
+        title = title.get_text().replace(" - Fap Roulette", "")
+        title = re.sub(r'[?|/:<>*\\]', '', title)
+
+    # checking if images is not zero
+    if len(images) != 0:
+        if len(images) == 1:
+            return False, False
+        if len(images) < 2:
+            print(images)
+            return False, False
+        image = images[2]
+        try:
+            # In image tag ,searching for "data-srcset"
+            image_link = image["data-srcset"]
+        # then we will search for "data-src" in img
+        # tag and so on..
+        except:
+            try:
+                # In image tag ,searching for "data-src"
+                image_link = image["data-src"]
+            except:
+                try:
+                    # In image tag ,searching for "data-fallback-src"
+                    image_link = image["data-fallback-src"]
+                except:
+                    try:
+                        # In image tag ,searching for "src"
+                        image_link = image["src"]
+                    # if no Source URL found
+                    except:
+                        return False, False
+
+        return title, image_link
+    return False, False
