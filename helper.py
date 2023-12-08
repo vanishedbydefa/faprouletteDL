1b1addec9ceb8fa7db889704fb2b0362352e2319
vanishedbydefa <128320031+vanishedbydefa@users.noreply.github.com>
2023-12-08 12:59:55 +0100

    Update helper.py

--- helper.py
+++ helper.py
@@ -13,7 +13,9 @@ def get_timestamp():
     return time.time()
 
 def create_urls(url_from:int, url_to:int):
-    if url_from <= 10:
+    if url_from == None:
+        url_from = 0
+    elif url_from <= 10:
         url_from = 0
     else:
         url_from -= 10
@@ -107,3 +109,59 @@ def process_url(url:str, proxie:dict):
         return title, image_link
     print("debug 1")
     return False, False
+
+def exe_helper():
+    path = input("Enter path where to store the images: ")
+    path = str(path)
+    print("Path set to: ", path)
+
+    while True:
+        try:
+            threads = input("Enter a number of threads to download [1-10]: ")
+            threads = int(threads)
+            if threads <= 0 or threads >10:
+                print("Invalid input! Please enter a number between 1 and 10")
+                continue
+            print("Threads set to: ", threads)
+            break
+        except ValueError:
+            print("Invalid input! Please enter a number between 1 and 10")
+
+    while True:
+        force = input("Do you want to force download even an image may already exist? [y/n]: ")
+        if force not in ["y", "Y", "j", "J", "yes", "Yes", "n", "N", "no", "No"]:
+            print("Type 'y' to force download or 'n' to do not: ")
+            continue
+        elif force in ["y", "Y", "j", "J", "yes", "Yes"]:
+            force = True
+            break
+        elif force in ["n", "N", "no", "No"]:
+            force = False
+            break
+    
+    while True:
+        beginning = input("Do you want to start downloading from 0? [y/n]: ")
+        if beginning not in ["y", "Y", "j", "J", "yes", "Yes", "n", "N", "no", "No"]:
+            print("Type 'y' to not continue where you stoped last time or 'n' to do so: ")
+            continue
+        elif beginning in ["y", "Y", "j", "J", "yes", "Yes"]:
+            beginning = True
+            break
+        elif beginning in ["n", "N", "no", "No"]:
+            beginning = False
+            break
+
+    while True:
+        proxie = input("Do you want to use a proxie? [y/n]: ")
+        if proxie not in ["y", "Y", "j", "J", "yes", "Yes", "n", "N", "no", "No"]:
+            print("Type 'y' to force download or 'n' to do not: ")
+            continue
+        elif proxie in ["y", "Y", "j", "J", "yes", "Yes"]:
+            proxie = input("Input proxies IP: ")
+            break
+        else:
+            proxie = None
+            break
+    return path, threads, force, beginning, proxie
+        
+
