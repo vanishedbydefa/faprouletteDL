38547c18a44b0507c2589f848a81c8e8a38d6109
vanishedbydefa <128320031+vanishedbydefa@users.noreply.github.com>
2024-07-01 15:49:28 +0200

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,18 +1,30 @@
 import os
+import logging
 import time
 import re
 import requests
 from bs4 import *
 
+from logger import logger
 from database import check_db_exists, get_all_ids_from_db
-
-def get_time():
-    return time.strftime('[%H:%M:%S]')
 
 def get_timestamp():
     return time.time()
 
+def sanitize_filename(input_string):
+    '''
+    Replace characters to get valid file
+    names. Only a few chars are allowed.
+    '''
+    pattern = re.compile(r'[^A-Za-z0-9\_\-\.]')
+    result_string = pattern.sub('', input_string)
+    return result_string
+
 def create_urls(url_from:int, url_to:int, db_source_path:str, speed=False):
+    '''
+    Create a list of lists containing the
+    image id and the roulettes url.
+    '''
     if url_from == None:
         url_from = 0
     elif url_from <= 10:
@@ -31,8 +43,7 @@ def check_path_exists(path:str, create=False):
     If path do not exist but create=True,
     create a directory.
     '''
-    path_exist = os.path.exists(path)
-    if path_exist:
+    if os.path.exists(path):
         return True
 
     #path do not exist, try to create it
@@ -41,33 +52,32 @@ def check_path_exists(path:str, create=False):
             os.makedirs(path)
             return True
         except OSError as e:
-            print(f"Failed to create directory: {path}")
-            print(f"Error: {e}")
-            return False
+            logger.error(f"Failed to create directory: {path}")
+            logger.error(f"Error: {e}")
     return False
 
 
 def initial_checks(param_path:str, db_path:str, db_path_source:str, speed=False, exe=False):
     ##check specified path to folder and create if it do not exist
     if check_path_exists(param_path, create=True):
-        print(f'    - Folder path at {param_path} is valid')
+        logger.info(f'    * Folder path {param_path} is valid')
     else:
-        print(f"\nError:\nThe folder where to store downloaded images "
-            f"do not exist and couldn't be created. Please check \n"
-            f"the correctness of the provided path: "
+        logger.error(f"\nError:\nThe folder where to store downloaded images ",
+            f"do not exist and couldn't be created. Please check \n",
+            f"the correctness of the provided path: ",
             f"{param_path}.")
         if exe:
-            time.sleep(60)
+            os.system("pause")
         exit(1)
     
     ##check if the db exist and create if it do not exist
     check_db_exists(db_path)
     if speed:
         if not check_path_exists(db_path_source, create=False):
-            print("\nERROR You want to use speed mode but there is no source database: 'image_data_source.db'")
-            print(f"      Make sure to place 'image_data_source.db' here: {db_path_source}")
+            logger.error("\nERROR You want to use speed mode but there is no source database: 'image_data_source.db'\n",
+                        f"      Make sure to place 'image_data_source.db' here: {db_path_source}")
             if exe:
-                time.sleep(60)
+                os.system("pause")
             exit(1)
 
 def process_url(url:str, proxie:dict):
@@ -93,7 +103,7 @@ def process_url(url:str, proxie:dict):
         if len(images) == 1:
             return False, False
         if len(images) < 2:
-            print(images)
+            logger.info(images)
             return False, False
         image = images[2]
         try:
@@ -118,30 +128,33 @@ def process_url(url:str, proxie:dict):
                         pass
 
         return title, image_link
-    print("debug 1")
+    logger.error(f"Unknown error for {url}")
     return False, False
 
 def exe_helper():
-    path = input("Enter path where to store the images: ")
-    path = str(path)
-    print("Path set to: ", path)
+    while True:
+        path = input("Enter a path where to store the images: ")
+        if os.path.isdir(path):
+            break
+        logger.error(f"{path} isn't a valid path")
+    logger.info(f"Path set to: {path}")
 
     while True:
         try:
-            threads = input("Enter a number of threads to download [1-10]: ")
+            threads = input("Enter a number of threads to use for downloading [1-10]: ")
             threads = int(threads)
             if threads <= 0 or threads >10:
-                print("Invalid input! Please enter a number between 1 and 10")
+                logging.error("Invalid input! Please enter a number between 1 and 10")
                 continue
-            print("Threads set to: ", threads)
+            logger.info("Threads set to: ", threads)
             break
         except ValueError:
-            print("Invalid input! Please enter a number between 1 and 10")
+            logger.error("Invalid input! Please enter a number between 1 and 10")
 
     while True:
         force = input("Do you want to force download even an image may already exist? [y/n]: ")
         if force not in ["y", "Y", "j", "J", "yes", "Yes", "n", "N", "no", "No"]:
-            print("Type 'y' to force download or 'n' to do not: ")
+            logger.error("Type 'y' to force download or 'n' to do not: ")
             continue
         elif force in ["y", "Y", "j", "J", "yes", "Yes"]:
             force = True
@@ -153,7 +166,7 @@ def exe_helper():
     while True:
         beginning = input("Do you want to start downloading from 0? [y/n]: ")
         if beginning not in ["y", "Y", "j", "J", "yes", "Yes", "n", "N", "no", "No"]:
-            print("Type 'y' to not continue where you stoped last time or 'n' to do so: ")
+            logger.error("Type 'y' to not continue where you stoped last time or 'n' to do so: ")
             continue
         elif beginning in ["y", "Y", "j", "J", "yes", "Yes"]:
             beginning = True
@@ -165,7 +178,7 @@ def exe_helper():
     while True:
         speed = input("Do you want to start downloading in speed mode? [y/n]: ")
         if speed not in ["y", "Y", "j", "J", "yes", "Yes", "n", "N", "no", "No"]:
-            print("Type 'y' to download based on an existing db otherwise type 'n': ")
+            logger.error("Type 'y' to download based on an existing db otherwise type 'n': ")
             continue
         elif speed in ["y", "Y", "j", "J", "yes", "Yes"]:
             speed = True
@@ -177,7 +190,7 @@ def exe_helper():
     while True:
         proxie = input("Do you want to use a proxie? [y/n]: ")
         if proxie not in ["y", "Y", "j", "J", "yes", "Yes", "n", "N", "no", "No"]:
-            print("Type 'y' to force download or 'n' to do not: ")
+            logger.error("Type 'y' to force download or 'n' to do not: ")
             continue
         elif proxie in ["y", "Y", "j", "J", "yes", "Yes"]:
             proxie = input("Input proxies IP: ")
