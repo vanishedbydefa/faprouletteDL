f0e0152d385d2fc71ddff09bcbe627f27c4087f8
vanishedbydefa <128320031+vanishedbydefa@users.noreply.github.com>
2024-07-01 15:49:13 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,8 @@
 import sqlite3
 import os
 
+from logger import logger
+
 def get_all_ids_from_db(db_path:str):
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
@@ -24,15 +26,15 @@ def get_max_id_from_db(db_path:str):
     if highest_id_downloaded == None and highest_id_general == None:
         return None
     elif highest_id_downloaded == None or highest_id_general == None:
-        print("ERROR: It seems like one table in the database is missing.")
-        print("This should not occure from normal usage of 'faproulette-downloader'.")
-        print("The program itself will not fix this issue. Think about deleting your DB and start downloading from zero again")
-        print("You can also download an existing DB from github and use the downloader in speed mode.")
+        logger.error("It seems like one table in the database is missing.\n",
+                    "This should not occure from normal usage of 'faproulette-downloader'.\n",
+                    "The program itself will not fix this issue. Think about deleting your DB and start downloading from zero again\n",
+                    "You can also download an existing DB from github and use the downloader in speed mode.")
     elif highest_id_downloaded < highest_id_general:
-        print(f"highest ID: {highest_id_general}")
+        logger.info(f"highest ID: {highest_id_general}")
         return highest_id_general
     else:
-        print(f"highest ID: {highest_id_downloaded}")
+        logger.info(f"highest ID: {highest_id_downloaded}")
         return highest_id_downloaded
 
 
@@ -76,7 +78,7 @@ def insert_or_update_entry(db_path:str, id_value:int , name:str, category:str, d
     conn.close()
 
 # Function to create a new database if it doesn't exist
-def create_new_db(db_path):
+def create_new_db(db_path:str):
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     cursor.execute('''
@@ -92,10 +94,10 @@ def create_new_db(db_path):
     conn.close()
     
 # Function to check if the database exists
-def check_db_exists(db_path):
-    if not os.path.exists(db_path):
-        print("    - Database not found. Creating a new one...")
+def check_db_exists(db_path:str):
+    if not os.path.isfile(db_path):
+        logger.info("    * Database not found. Creating a new one...")
         create_new_db(db_path)
-        print("    - Database created at", db_path)
+        logger.info(f"    * Database created at {db_path}")
     else:
-        print("    - Database already exists at", db_path)
+        logger.info(f"    * Database already exists at {db_path}")
